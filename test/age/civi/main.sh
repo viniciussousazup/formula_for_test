8aa747d1262fd9867cf77bc66faf326e213dd892
Daniel Barra <dmbarra@gmail.com>
2021-01-19 13:02:52 -0300

    Change formula

---
+++ test/age/civi/main.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. ./src/get-civi.sh --source-only
+
+run $SAMPLE_TEXT $SAMPLE_LIST $SAMPLE_BOOL
---
+++ test/age/civi/src/get-civi.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+run() {
+  echo "send request"
+  curl -X GET "https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/4" -H "accept: application/json"
+  echo "done"
+}
