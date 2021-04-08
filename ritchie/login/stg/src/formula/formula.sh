9eb18215936826f8598c16079109e1e852dcaa46
Daniel Barra <dmbarra@gmail.com>
2021-04-08 14:28:01 -0300

    Change raw to binary

--- ritchie/login/stg/src/formula/formula.sh
+++ ritchie/login/stg/src/formula/formula.sh
@@ -5,7 +5,7 @@ runFormula() {
   curl --location --request POST 'https://dennis.stgdennis.zup.io/login' \
   --header 'Content-Type: application/json' \
   --header 'x-org: default' \
-  --data-raw '{ "username":"'$USERNAME'", "password":"'$PASSWORD'" }'
+  --data-binary "{ \"username\":\"$USERNAME\", \"password\":\"$PASSWORD\" }"
 
   echo "done"
 }
