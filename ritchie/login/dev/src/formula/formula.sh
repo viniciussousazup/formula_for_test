48e1e8fa2e135ad3dc41c41ff09d13915b68870f
Daniel Barra <dmbarra@gmail.com>
2021-04-08 13:53:46 -0300

    Change curl

--- ritchie/login/dev/src/formula/formula.sh
+++ ritchie/login/dev/src/formula/formula.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 runFormula() {
   echo "send request"
+
   curl --location --request POST 'https://dennis.devdennis.zup.io/login' \
   --header 'Content-Type: application/json' \
   --header 'x-org: default' \
-  --data-raw '{ "username":"'$USERNAME'", "password":"'$PASSWORD'" }'
+  --data-raw '{
+      "username":"'$USERNAME'",
+      "password":"'$PASSWORD'"
+  }'
+
   echo "done"
 }
